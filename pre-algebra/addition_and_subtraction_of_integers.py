795643fe77c3e825be98aa734e67078d6c891d99
Will <will@wiri.co.uk>
2023-05-12 11:05:28 +0100

    refacted into a class, WIP

--- pre-algebra/addition_and_subtraction_of_integers.py
+++ pre-algebra/addition_and_subtraction_of_integers.py
@@ -1,24 +1,54 @@
+
+
+##OPERATORS = ['-','+']
+##
+##expression_list = []
+##
+##expression_list.append(str(random.randrange(-9,9)))
+##for i in range(random.randrange(1,4)):
+##    expression_list.append(random.choice(OPERATORS))
+##    expression_list.append(str(random.randrange(-9,9)))
+##
+##
+##expression = ''
+##for item in expression_list:
+##    if '-' in item and len(item) > 1:
+##        item = '(' + item + ')'
+##    if item == '-' or item == '+':
+##        item = ' ' + item + ' '
+##
+##    expression += item
+##
+##
+##print(expression)
+##print(eval(expression))
 import random
 
-OPERATORS = ['-','+']
+EXPONENTS = [2,3,4]
+NUMBER_RANGE = 9
 
-expression_list = []
+class Expression:
+    """generates a maths expression based on given parameters"""
+    def __init__(self,
+                 number_range = NUMBER_RANGE,
+                 include_addition = True,
+                 include_subtraction = True,
+                 include_multiplication = True,
+                 include_division = True,
+                 include_exponents = False,
+                 include_variables = False
+                 ):
+        #addition, subtraction, multiplication, division
 
-expression_list.append(str(random.randrange(-9,9)))
-for i in range(random.randrange(1,4)):
-    expression_list.append(random.choice(OPERATORS))
-    expression_list.append(str(random.randrange(-9,9)))
+        self.expression = self.generate_expression()
+        self.result = self.calculate_expression()
 
+    def generate_expression(self):
+        pass
 
-expression = ''
-for item in expression_list:
-    if '-' in item and len(item) > 1:
-        item = '(' + item + ')'
-    if item == '-' or item == '+':
-        item = ' ' + item + ' '
-
-    expression += item
-
-
-print(expression)
-print(eval(expression))
+    def calculate_expression(self):
+        pass
+    
+a = Expression()
+print(a.expression)
+print(a.result)
