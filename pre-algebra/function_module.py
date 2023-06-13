8149cdc800502353b156e622f44f11f7a3261a1c
Will <will@wiri.co.uk>
2023-06-13 10:25:28 +0100

    added function library for code re-use

---
+++ pre-algebra/function_module.py
@@ -0,0 +1,30 @@
+def gen_multiplication_table(calculation_limit, highest_times_table):
+    """returns non-duplicating list of products up to the calcuation limit, including all times-tablesup to highest_times_table""" 
+    multi_table = []
+
+    for i in range(2, highest_times_table + 1):
+        for j in range(1, calculation_limit):
+            result = i * j
+
+            if result > calculation_limit or result in multi_table:
+                break
+            else:
+                multi_table.append(result)
+
+    return sorted(multi_table)
+
+
+def gen_prime_number_list(limit):
+    """returns list of prime numbers up to limit"""
+    list_of_primes = []
+
+    for candidate_prime in range(3, limit + 1, 2):
+        divisors = 0
+        for j in range(1, candidate_prime + 1):
+            if candidate_prime % j == 0:
+                divisors += 1
+
+        if divisors <= 2:
+            list_of_primes.append(candidate_prime)
+    
+    return sorted(list_of_primes)
